954dc6101e7137b4014f497d876af65666a7d98a
Ihor Karen <karenihor@gmail.com>
2023-02-28 21:36:43 +0200

    search logic

--- src/index.js
+++ src/index.js
@@ -4,17 +4,71 @@ const debounce = require('lodash.debounce');
 
 const DEBOUNCE_DELAY = 300;
 
+const formInputEl = document.querySelector('[id="search-box"]');
+const listEl = document.querySelector('.country-list');
+const counrtyInfoEl = document.querySelector('.country-info');
 
-const formInputEl = document.querySelector('[id="search-box"]')
+formInputEl.addEventListener('input', debounce(onInputSearch, DEBOUNCE_DELAY));
 
-formInputEl.addEventListener('input', debounce(onInputSearch, DEBOUNCE_DELAY))
+const responseFiler = '?fields=name,capital,population,flags,languages';
 
 function onInputSearch(e) {
-    e.preventDefault();
-    
-    console.log(formInputEl.value);
+  e.preventDefault();
+
+  const formValue = formInputEl.value;
+  fetchCountries(formValue);
 }
 
 function fetchCountries(name) {
+  return fetch(`https://restcountries.com/v3.1/name/${name}${responseFiler}`)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(
+          Notiflix.Notify.failure('Oops, there is no country with that name')
+        );
+      }
+      return response.json();
+    })
+    .then(countries => {
+      return onUpdateUI(countries);
+    })
+    .catch(error => console.log(error));
+}
 
+function onUpdateUI(params) {
+  let markup = '';
+  counrtyInfoEl.innerHTML = '';
+  listEl.innerHTML = '';
+
+  if (params.length > 10) {
+    Notiflix.Notify.info(
+      '"Too many matches found. Please enter a more specific name."'
+    );
+    return;
+  }
+
+  if (params.length > 1) {
+    markup = params
+      .map(country => {
+        return `<li><img src=${country.flags.svg} width="30"/><p>${country.name.official}</p></li>`;
+      })
+      .join('');
+
+    return listEl.insertAdjacentHTML('afterbegin', markup);
+  }
+
+  if (params.length === 1) {
+    markup = params
+      .map(country => {
+        return `<span><img src=${country.flags.svg} width="30"/></span><p>${
+          country.name.official
+        }</p>
+        <ul><li>${country.capital}</li><li>${
+          country.population
+        }</li><li>${Object.values(country.languages)}</li></ul>`;
+      })
+      .join('');
+
+    return counrtyInfoEl.insertAdjacentHTML('afterbegin', markup);
+  }
 }
