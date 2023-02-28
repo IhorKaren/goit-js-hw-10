d592b08d96346db8047c17657e5a23007c37bdbe
Ihor Karen <karenihor@gmail.com>
2023-02-28 19:56:57 +0200

    test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 import './css/styles.css';
+import Notiflix from 'notiflix';
+const debounce = require('lodash.debounce');
 
 const DEBOUNCE_DELAY = 300;
+
+
+const formInputEl = document.querySelector('[id="search-box"]')
+
+formInputEl.addEventListener('input', debounce(onInputSearch, DEBOUNCE_DELAY))
+
+function onInputSearch(e) {
+    e.preventDefault();
+    
+    console.log(formInputEl.value);
+}
+
+function fetchCountries(name) {
+
+}
